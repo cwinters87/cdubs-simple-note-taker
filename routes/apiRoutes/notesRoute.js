6389d581b64cbb9fb5d03996c6595f71589c7919
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-06-02 15:00:36 -0500

    trying to deploy app

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -11,7 +11,6 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
-  console.log(req.body)
   const note = createNote(req.body, notes);
   res.json(note);
 });
